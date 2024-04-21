d63a5b191ec1e19cb501d308bc0f1ed83853bb14
matteocenci <matteocenci01@gmail.com>
2024-04-21 16:01:40 +0200

    feat: trying methods

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log(Vue);
+const app = Vue.createApp ({
+    data () {
+        return {
+            newToDo: '',
+
+
+        }
+    },
+
+    methods: {
+        addToDo() {
+            if (this.newToDo.trim() !== '') {
+                this.todos.push({ text: this.newToDo, done: false});
+                this.saveTodo();
+                this.newToDo = '';
+            }
+        },
+        toggleToDo(index) {
+            this.todos[index].done = !this.todos[index].done;
+            this.saveTodo();
+        },
+        removeToDo(index) {
+            this.todos(index, 1);
+            this.saveToDo();
+        },
+        
+    }
+});
+app.mount('#app');
